2f2e81803f2169311d03a03a62dce1d7ae8d2305
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2019-04-24 15:14:30 +0000

    Consegna Form

---
+++ Form/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+
+import {
+ FormsModule,
+ ReactiveFormsModule
+} from '@angular/forms';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+   ReactiveFormsModule,
+   NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Form/src/app/form-demo/form-demo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-demo',
+  templateUrl: './form-demo.component.html',
+  styleUrls: ['./form-demo.component.css']
+})
+export class FormDemoComponent implements OnInit {
+  myForm: FormGroup;
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['ABC123']
+    });
+  }
+
+
+  ngOnInit() {
+  }
+
+}
