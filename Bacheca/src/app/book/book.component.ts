63b5d346c04b54ba4d8e9261e7744874c238deed
Wu-Chong <wu.chong.lin@iisgalvanimi.edu.it>
2019-04-25 13:13:08 +0000

    Da finire

--- Bacheca/src/app/book/book.component.ts
+++ Bacheca/src/app/book/book.component.ts
@@ -9,11 +9,18 @@ import {FormBuilder, FormGroup,Validators} from '@angular/forms';
 })
 export class BookComponent implements OnInit {
 
+  
+
     myForm: FormGroup;
     constructor(fb: FormBuilder) {
       this.myForm = fb.group({
         'nome': ['Inserisci nome', Validators.required],
-        'cognome': ['Inserisci cognome', Validators.required]
+        'cognome': ['Inserisci cognome', Validators.required],
+        'indirizzo': ['Inserisci indirizzo', Validators.required],
+        'phone': ['Inserisci telefono', Validators.required],
+        'email': ['Inserisci email', Validators.required],
+        'preData': ['Data Prenotazione', Validators.required],
+        'preOra': ['Ora Prenotazione', Validators.required]
     });
  }
 
